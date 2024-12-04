c614f3109451d316c90ae93b1d503fe8804bbd4f
bahaaCs <bahaaaboukhalil@gmail.com>
2024-12-04 21:22:01 +0200

    creating a sidebar

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const SideBar = ()=>{
+    return (
+        <div className="side-bar flex column primary-bg">
+            <h3>Build Up</h3>
+            <p>Quizes</p>
+            <p>Profile</p>
+            <p>Settings</p>
+
+        </div>
+    )
+}
+
+export default SideBar;
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import QuizCard from "../components/QuizCard";
+import SideBar from "../components/SideBar";
 
 const Quiz = () => {
     return (
-        <div className="quiz-page flex column center">
+        <div className="quiz-page flex row">
+            <SideBar />
             <QuizCard />
         </div>
     )
