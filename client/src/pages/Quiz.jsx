b9d5c085005d6d5a0ce96a93129498fa8deb8a73
bahaaCs <bahaaaboukhalil@gmail.com>
2024-12-08 00:02:55 +0200

    frontend <> getting id of the quiz + craeting a map to list the questions (still need to specify the id)

--- client/src/pages/Quiz.jsx
+++ client/src/pages/Quiz.jsx
@@ -1,70 +1,78 @@
 import React, {useState, useContext} from "react";
 import { QuizzesContext } from "../contexts/quizzesContext";
-
+import { useLocation } from "react-router-dom";
 const Quiz = () => {
+    const location = useLocation();
     const [answers, setAnswers] = useState({});
     const [score, setScore] = useState(0);
     const [selectedOption, setSelectedOption] = useState("");
-    
+    const {quizzes} = useContext(QuizzesContext);
+
+    const quizId = location.pathname.split("/").pop();
+
     const handleChange = (event) => {
       setSelectedOption(event.target.value);
     };
 
     const listQuizzes=()=>{
-
-    }
-
-    return (
-        <div className="quiz flex column">
-            <h3 className="black-txt">Qustion number</h3>
+        quizzes.map((quiz, index)=>{
+            <div className="quiz flex column">
+            <h3 className="black-txt">Qustion {index}</h3>
             <div className="question-container primary-bg">
-                <p>What is the output of `print(2 + 3 * 4)` in Python?</p>
+                <p>{quiz.question}</p>
             </div>
             <div className="options-container flex column">
                 <label>
                     <input
                     type="radio"
                     name="q1"
-                    value="a"
-                    checked={selectedOption === "a"}
+                    value={quiz.options[0]}
+                    checked={selectedOption === quiz.options[0]}
                     onChange={handleChange}
                     />
-                    A
+                    {quiz.options[0]}
                 </label>
                 <label>
                     <input
                         type="radio"
-                        name="q1"
-                        value="b"
-                        checked={selectedOption === "b"}
+                        name={index}
+                        value={quiz.options[1]}
+                        checked={selectedOption === quiz.options[1]}
                         onChange={handleChange}
                     />
-                    B
+                    {quiz.options[1]}
                 </label>
                 <label>
                     <input
                         type="radio"
-                        name="q1"
-                        value="c"
-                        checked={selectedOption === "c"}
+                        name={index}
+                        value={quiz.options[2]}
+                        checked={selectedOption === quiz.options[2]}
                         onChange={handleChange}
                     />
-                    C
+                    {quiz.options[2]}
                 </label>
                 <label>
                     <input
                         type="radio"
-                        name="q1"
-                        value="d"
-                        checked={selectedOption === "d"}
+                        name={index}
+                        value={quiz.options[3]}
+                        checked={selectedOption === quiz.options[3]}
                         onChange={handleChange}
                     />
-                    D
+                    {quiz.options[3]}
                 </label>
 
                 <p>Selected Option: {selectedOption}</p>
             </div>
 
+        </div>
+        })
+    }
+
+    return (
+        <div className="quizzes-container flex column">
+            {listQuizzes}
         </div>
     );
 };
