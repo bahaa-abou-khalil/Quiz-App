b1ddeeb741c53f2addbc24083c9c9d391725ec8a
bahaaCs <bahaaaboukhalil@gmail.com>
2024-12-07 03:47:09 +0200

    backend <> updating index.js for quiz routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import connectToDatabase from "./db/connection.js";
 import userRoutes from "./routes/users.routes.js";
+import quizRoutes from "./routes/quizzes.routes.js";
+
 const app = express();
 
 app.use(express.json());
 
 app.use("/users",userRoutes);
+app.use("/quizes",quizRoutes);
 
 app.listen(8080, async () => {
   console.log("Server running on port 8080");
