d55f25a94ec8a556111eaba0d861545edaa8a228
bahaaCs <bahaaaboukhalil@gmail.com>
2024-12-08 11:50:17 +0200

    frontend <> editing login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,27 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useNavigate } from "react-router-dom";
+import useForm from "../hooks/useForm";
 
 const Login = ()=>{
+    const {form, updateForm} = useForm({
+        username:"",
+        password:""
+    })
     const navigate=useNavigate();
+
     return(
         <div className="login-container primary-bg flex column center">
             <h3>Login</h3>
-            <input type="text" placeholder="username"/>
-            <input type="password" placeholder="password"/>
+            <input type="text" placeholder="username"
+            value={form.username}
+            name="username"
+            onChange={updateForm}
+            />
+            <input type="password" placeholder="password"
+            value={form.password} 
+            name="password"
+            onChange={updateForm}
+            />
             <button className="filled-btn"
             onClick={()=>navigate("/quizzes")}
             >Submit</button>
