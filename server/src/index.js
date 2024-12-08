6d91ef8fc5b6c9f2f1e7ddbad7001fb07e878146
bahaaCs <bahaaaboukhalil@gmail.com>
2024-12-08 12:14:14 +0200

    backend <> creating middlware

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,11 @@
 import express from "express";
+import cors from 'cors';
 import connectToDatabase from "./db/connection.js";
 import userRoutes from "./routes/users.routes.js";
 import quizRoutes from "./routes/quizzes.routes.js";
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/users",userRoutes);
